fd1d5cfb9f514e330de263c9b5f32b0ce4764891
PAIPURIGNANAPRASUNA <133932760+PAIPURIGNANAPRASUNA@users.noreply.github.com>
2023-05-19 10:27:11 +0530

    Add files via upload

---
+++ SpringBootRestApiJdbcCurdOperationApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrestapi.SpringBootRestAPIJDBCCurdOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiJdbcCurdOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiJdbcCurdOperationApplication.class, args);
+	}
+
+}
